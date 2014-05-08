b5c43462f84c4ac7a67bca4147a1231676ce2fa8
Scott Kirkland <srkirkland@gmail.com>
2014-05-08 16:00:45 -0700

    customer info stuff added

---
+++ Cybersource/Controllers/OderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cybersource.Controllers
+{
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// Get the customer info for a new order
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CustomerInfo()
+        {
+            return View();
+        }
+	}
+}
---
+++ Cybersource/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cybersource.MvcApplication" Language="C#" %>
