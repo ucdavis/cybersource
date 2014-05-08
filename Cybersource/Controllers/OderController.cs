b55eb2f1db2a06fdc5e7c4b9039b1d0f0a0d432d
Scott Kirkland <srkirkland@gmail.com>
2014-05-08 16:40:02 -0700

    basic cybersource payment workflow

--- Cybersource/Controllers/OderController.cs
+++ Cybersource/Controllers/OderController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cybersource.Helpers;
 
 namespace Cybersource.Controllers
 {
@@ -14,6 +16,21 @@ namespace Cybersource.Controllers
         /// <returns></returns>
         public ActionResult CustomerInfo()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Payment()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            foreach (var param in Request.Form.AllKeys)
+            {
+                dictionary.Add(param, Request.Form[param] ?? string.Empty);
+            }
+
+            ViewBag.Signature = Security.sign(dictionary);
+
             return View();
         }
 	}
