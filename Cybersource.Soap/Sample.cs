2206f04da47327bd690306730d98c0d78f97c896
John Knoll <jpknoll@ucdavis.edu>
2014-06-16 11:06:21 -0700

    Update sample filename

--- Cybersource.Soap/Sample.cs
+++ Cybersource.Soap/Sample.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Cybersource.Soap.Transaction.Xml;
 
 namespace Cybersource.Soap
 {
-    public class Class1
+    public class Sample
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
