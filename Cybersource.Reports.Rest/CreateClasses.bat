41d4c9ddb85da546f145e04b9b5e47a00920b002
John Knoll <jpknoll@ucdavis.edu>
2014-06-17 15:30:04 -0700

    add sdr class

--- Cybersource.Reports.Rest/CreateClasses.bat
+++ Cybersource.Reports.Rest/CreateClasses.bat
@@ -1,2 +1,3 @@
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\per_1_2.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.PaymentEventsReport
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\sdr.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.SubscriptionDetailReport
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\tdr_1_13.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.TransactionDetailReport
