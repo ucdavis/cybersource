85831c2252ab6b876e3a44a84777b4d3216a2d70
John Knoll <jpknoll@ucdavis.edu>
2014-06-17 13:22:09 -0700

    new report def

--- Cybersource.Reports.Rest/CreateClasses.bat
+++ Cybersource.Reports.Rest/CreateClasses.bat
@@ -1,1 +1,2 @@
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\per_1_2.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.PaymentEventsReport
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\tdr_1_13.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.TransactionDetailReport
