8b628313f4d39ae54edce3dd0e94a6fd336da3a0
John Knoll <jpknoll@ucdavis.edu>
2014-06-17 13:54:50 -0700

    create service to manage report fetch

---
+++ Cybersource.Reports.Rest/ReportService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Cybersource.Reports.Rest.TransactionDetailReport;
+
+namespace Cybersource.Reports.Rest
+{
+    public class ReportService
+    {
+        private readonly const string _demandUri;
+
+        private readonly const string _queryUri;
+
+        public string MerchantId { get; set; }
+        public AuthenticationHeaderValue Credentials { get; set; }
+
+        public ReportService(string BaseUri)
+        {
+            _demandUri = "https://ebctest.cybersource.com/ebctest/DownloadReport/{year}/{month}/{day}/{merchantId}/{reportName}.{reportFormat}";
+            _queryUri = "https://ebctest.cybersource.com/ebctest/Query";
+        }
+
+        public async Task<TransactionDetailReport.Report> GetTransactionDetailReport(string transactionId)
+        {
+            // form data
+            var data = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("merchantID", MerchantId),
+                new KeyValuePair<string, string>("type", "transaction"),
+                new KeyValuePair<string, string>("subtype", "transactionDetail"),
+                new KeyValuePair<string, string>("requestID", transactionId),
+                new KeyValuePair<string, string>("versionNumber", "1.5")
+            });
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = Credentials;
+
+            var response = await client.PostAsync(_queryUri, data);
+
+            response.EnsureSuccessStatusCode();
+
+            // fetch content
+            var content = await response.Content.ReadAsStringAsync();
+
+            // fix namespaces
+            content = content.Replace("tdr_1_5.dtd", "tdr_1_13.dtd");
+            return DesearializeReport<TransactionDetailReport.Report>(content);
+        }
+
+        private static TReport DesearializeReport<TReport>(string xml) where TReport : class
+        {
+            // fix namespaces
+            xml = FixNamespace(xml);
+            var stream = new StringReader(xml);
+
+            // deserialize
+            var serializer = new XmlSerializer(typeof(TReport));
+            return serializer.Deserialize(stream) as TReport;
+        }
+
+        private static string FixNamespace(string xml)
+        {
+            return xml.Replace("https://ebctest.cybersource.com/ebctest/reports/dtd", "https://ebc.cybersource.com/ebc/reports/dtd");
+        }
+    }
+}
--- Cybersource.Reports.Rest/Sample.cs
+++ Cybersource.Reports.Rest/Sample.cs
@@ -12,9 +12,7 @@ namespace Cybersource.Reports.Rest
 {
     class Sample
     {
-        private const string BaseUri = "https://ebctest.cybersource.com/ebctest/DownloadReport/{year}/{month}/{day}/{merchantId}/{reportName}.{reportFormat}";
-
-        private const string QueryUri = "https://ebctest.cybersource.com/ebctest/Query";
+        
 
         static void Main(string[] args)
         {
@@ -27,38 +25,17 @@ namespace Cybersource.Reports.Rest
 
         static async void StaticReport()
         {
-            // uri construction
-            var yesterday = DateTime.Today - TimeSpan.FromDays(1);
-            var uriParts = new
+            var reportService = new ReportService("https://ebctest.cybersource.com/ebctest/")
             {
-                merchantId = "ucdavis_jpknoll",
-                reportName = "PaymentEventsReport",
-                reportFormat = "xml",
-                year = yesterday.ToString("yyyy"),
-                month = yesterday.ToString("MM"),
-                day = yesterday.ToString("dd")
+                Credentials = GetCredentials(),
+                MerchantId = "ucdavis_jpknoll"
             };
 
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = GetCredentials();
-
-            try 
-            { 
-                var response = await client.GetAsync(BaseUri.FormatWith(uriParts));
-
-                response.EnsureSuccessStatusCode();
-
-                // fetch content
-                var content = await response.Content.ReadAsStringAsync();
-
-                // fix namespaces
-                content = FixNamespace(content);
-                content = content.Replace("tdr_1_5.dtd", "tdr_1_13.dtd");
-                var stream = new StringReader(content);
-
-                // deserialize
-                var serializer = new XmlSerializer(typeof(PaymentEventsReport.Report));
-                var report = serializer.Deserialize(stream) as PaymentEventsReport.Report;
+            var yesterday = DateTime.Today - TimeSpan.FromDays(1);
+            
+            try
+            {
+                var report = reportService.GetPaymentEventsReport(yesterday);
 
                 Console.Write(report);
             }
@@ -70,36 +47,15 @@ namespace Cybersource.Reports.Rest
 
         static async void QueryReport()
         {
-            // form data
-            var data = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
+            var reportService = new ReportService("https://ebctest.cybersource.com/ebctest/")
             {
-                new KeyValuePair<string, string>("merchantID", "ucdavis_jpknoll"),
-                new KeyValuePair<string, string>("type", "transaction"),
-                new KeyValuePair<string, string>("subtype", "transactionDetail"),
-                new KeyValuePair<string, string>("requestID", "4029572250300176056442"),
-                new KeyValuePair<string, string>("versionNumber", "1.5")
-            });
-
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = GetCredentials();
+                Credentials = GetCredentials(),
+                MerchantId = "ucdavis_jpknoll"
+            };
 
             try
             {
-                var response = await client.PostAsync(QueryUri, data);
-
-                response.EnsureSuccessStatusCode();
-
-                // fetch content
-                var content = await response.Content.ReadAsStringAsync();
-
-                // fix namespaces
-                content = FixNamespace(content);
-                content = content.Replace("tdr_1_5.dtd", "tdr_1_13.dtd");
-                var stream = new StringReader(content);
-
-                // deserialize
-                var serializer = new XmlSerializer(typeof(TransactionDetailReport.Report));
-                var report = serializer.Deserialize(stream);
+                var report = reportService.GetTransactionDetailReport("4029572250300176056442");
 
                 Console.Write(report);
             }
@@ -109,10 +65,7 @@ namespace Cybersource.Reports.Rest
             }
         }
 
-        private static string FixNamespace(string xml)
-        {
-            return xml.Replace("https://ebctest.cybersource.com/ebctest/reports/dtd", "https://ebc.cybersource.com/ebc/reports/dtd");
-        }
+        
 
         private static AuthenticationHeaderValue GetCredentials()
         {
