b64d7aa8d3dcce3bfe3c802c188cf6a9898b4122
John Knoll <jpknoll@ucdavis.edu>
2014-09-30 11:26:13 -0700

    add subscription search

--- Cybersource.Reports.Rest/CreateClasses.bat
+++ Cybersource.Reports.Rest/CreateClasses.bat
@@ -2,5 +2,6 @@
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\per_1_2.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.PaymentEventsReport
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\psdr.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.PaymentSubmissionDetailReport
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\sdr.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.SubscriptionDetailReport
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\subscriptionsearch.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.SubscriptionSearchReport
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\tdr_1_13.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.TransactionDetailReport
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" xsd\tedr.xsd /classes /language:CS /out:classes /namespace:Cybersource.Reports.Rest.TransactionExceptionDetailReport
